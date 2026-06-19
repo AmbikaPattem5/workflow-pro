a69bfcab97c58cfb49528dec430a352b85b2a7a0
AmbikaPattem <ambikapattem5@gmail.com>
2026-06-19 15:46:27 +0530

    project

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import { BrowserRouter} from 'react-router-dom';
 import App from './App.tsx'
-
+import './practice.ts'
 ReactDom.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,5 +1,21 @@
+import {projects} from "../../services/projects.ts";
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
+import './Projects.css'
+
 function Projects(){
-    return <h1>Projects</h1>
+    console.log(projects);
+    return (
+        <div>
+            <h1>Projects</h1>
+            <div className="projects-container">
+                {
+                    projects.map(project=>
+                        <ProjectCard key={project.id} project={project}/>
+                    )
+                }
+            </div>
+        </div>
+    )
 }   
 
 export default Projects;
