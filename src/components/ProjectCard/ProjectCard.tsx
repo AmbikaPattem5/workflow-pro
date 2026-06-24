8af73ffd077d4560fd91d103604968368968fbe6
AmbikaPattem <ambikapattem5@gmail.com>
2026-06-24 23:15:01 +0530

    edit feature

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -4,13 +4,15 @@ import type { Project } from "../../types/project";
 type ProjectCardProps={
     project: Project;
     onDelete :(id:number)=>void,
+    onEdit:(id:number,name:string)=>void
 }
-function ProjectCard({project, onDelete}: ProjectCardProps){
+function ProjectCard({project, onDelete, onEdit}: ProjectCardProps){
     return(
         <div className="project-card">
             <h3>{project.name}</h3>
             <p>Status: {project.status}</p>
             <button onClick={()=>onDelete(project.id)}>Delete</button>
+            <button onClick={()=>onEdit(project.id, project.name)}>Edit</button>
         </div>
     )
 }
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -7,31 +7,54 @@ import './dashboard.css';
 function Dashboard(){
      const [projectName, setProjectName] = useState("");
      const [projects,setProjects]=useState<Project[]>(initialProjects);
+     const [editingProjectId, setEditingProjectId] = useState<number | null>(null);
+     
      const handleAddProject=()=>{
         if(!projectName.trim()){
             return;
         }
         console.log(projectName)
+        if(editingProjectId===null){
         const newProject: Project={
         id: Date.now(),
         name: projectName,
         status:"Pending"
-     };
-     setProjects([...projects,newProject]);
-     setProjectName("");
+        }     
+        setProjects([...projects,newProject]);
+        setProjectName("");
+        }
+        else{
+            const updatedProjects=projects.map(project=>{
+                if(project.id===editingProjectId){
+                    return {...project, name: projectName};
+                }
+                else{
+                    return project;
+                }
+            })
+            setProjects(updatedProjects);
+            setEditingProjectId(null);  
+            setProjectName("");
+        }
      console.log(projects);
      }
      const handleDeleteProject=(id:number)=>{
         const updatedProjects=projects.filter(project=>project.id!==id);
         setProjects(updatedProjects)
      }
-     
+     const handleEditProject=(id:number,name:string)=>{
+        {
+            setEditingProjectId(id);
+            setProjectName(name);
+        }
+        
+        }
     return (
         
         <div>
             <h1>Dashboard</h1>
             <input type="text" placeholder="Project Name" value={projectName} onChange={(e)=>setProjectName(e.target.value)}/>
-            <button onClick={handleAddProject}>Add Project</button>
+            <button onClick={handleAddProject}>{editingProjectId===null?"Add Project":"Save Changes"}</button>
             <div className="stats-container">
                 <StatCard title="Total Projects" value={12}/>
                 <StatCard title="Completed Tasks" value={84}/>
@@ -39,7 +62,7 @@ function Dashboard(){
             </div>
             <div className="projects-container">
                 {projects.map(project=>(
-                    <ProjectCard key={project.id} project={project} onDelete={handleDeleteProject}/>
+                    <ProjectCard key={project.id} project={project} onDelete={handleDeleteProject} onEdit={handleEditProject}/>
                 ))}
             </div>
         </div>
